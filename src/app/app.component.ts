a1507213e49c01a9fa02d332b93ca23da7c49e77
DavidClase <dzamoramartinez@safareyes.es>
2024-10-24 15:52:50 +0200

    terminamos los ejercicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {Ejercicio5Component} from './ejercicio5/ejercicio5.component';
 import {Ejercicio5ParentComponent} from './ejercicio5-parent/ejercicio5-parent.component';
 import {Ejercicio6Component} from './ejercicio6/ejercicio6.component';
 import {Ejercicio7Component} from './ejercicio7/ejercicio7.component';
+import {Ejercicio8Component} from './ejercicio8/ejercicio8.component';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,7 @@ import {Ejercicio7Component} from './ejercicio7/ejercicio7.component';
     Ejercicio5ParentComponent,
     Ejercicio6Component,
     Ejercicio7Component,
+    Ejercicio8Component,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/ejercicio8/ejercicio8.component.ts
+++ src/app/ejercicio8/ejercicio8.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
+import {CurrencyPipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-ejercicio8',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    NgForOf
+  ],
   templateUrl: './ejercicio8.component.html',
   styleUrl: './ejercicio8.component.css'
 })
 export class Ejercicio8Component {
   productos = [
-    { nombre: 'Libro', precio: 20 },
-    { nombre: 'Lapiz', precio: 2 }
+    { nombre: 'Libro', precio: 20, cantidad: 1 },
+    { nombre: 'Lapiz', precio: 2, cantidad: 5 },
   ];
+
+  precioTotal() {
+    let total = 0;
+    for (let producto of this.productos) {
+      total += producto.precio * producto.cantidad;
+    }
+    return total;
+  }
+
+  suma1producto(producto: any) {
+    let id:number = this.productos.indexOf(producto);
+    this.productos[id].cantidad++;
+    this.precioTotal();
+  }
+
+  resta1producto(producto: any) {
+    let id:number = this.productos.indexOf(producto);
+    this.productos[id].cantidad--;
+    this.precioTotal();
+  }
+
 
 
 
